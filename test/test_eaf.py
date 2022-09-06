9568f56961b1f799871ebe955b6ad6656ecaf098
nabenabe0928 <shuhei.watanabe.utokyo@gmail.com>
2022-09-06 10:57:51 +0900

    [test] Add more precise tests for attainment computations

--- test/test_eaf.py
+++ test/test_eaf.py
@@ -1,3 +1,4 @@
+import itertools
 import unittest
 
 import numpy as np
@@ -11,8 +12,46 @@ from eaf.eaf import (
 )
 
 
+def func(X: np.ndarray) -> np.ndarray:
+    f1 = np.sum(X**2, axis=-1)
+    f2 = np.sum((X - 2) ** 2, axis=-1)
+    return np.stack([f1, f2], axis=-1)
+
+
+def get_dummy_dataset() -> np.ndarray:
+    dim, n_samples, n_trials = 2, 20, 10
+    X = np.random.random((n_trials, n_samples, dim)) * 10 - 5
+    costs = func(X)
+    return costs
+
+
 def test_compute_emp_att_surf() -> None:
-    pass
+    costs = get_dummy_dataset()
+    pf_set_list = _get_pf_set_list(costs)
+    costs = costs.reshape(20 * 10, 2)
+    order = np.argsort(costs[:, 0])
+    costs = costs[order]
+    level = 5
+    sol = _compute_emp_att_surf(X=costs[:, 0], pf_set_list=pf_set_list, level=level)
+    for p in sol:
+        if p[-1] == np.inf:
+            continue
+
+        cnt = 0
+        for pf_set in pf_set_list:
+            attained = np.any(np.all(pf_set <= p, axis=-1))
+            print(pf_set, p, attained)
+            cnt += attained
+        assert cnt >= level
+
+    for p in itertools.product(costs[:, 0], costs[:, 1]):
+        cnt = 0
+        point = np.asarray(p)
+        for pf_set in pf_set_list:
+            attained = np.any(np.all(pf_set <= point, axis=-1))
+            cnt += attained
+        if cnt >= level:
+            assert np.any(np.all(p == point, axis=-1))
 
 
 def test_get_pf_set_list() -> None:
